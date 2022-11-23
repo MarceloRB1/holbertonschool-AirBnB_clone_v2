0d5ffccd015861793d395286b3d91cbf021430af
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2022-11-23 20:49:50 -0300

    edit def all -- task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
